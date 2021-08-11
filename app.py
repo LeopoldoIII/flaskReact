cb96454b25c32de0ae687fbbf98b4097d3a36e7a
LeopoldoIII <nathan.rx@gmail.com>
2021-08-10 21:00:57 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def hello_world():
+    request_method = request.method
+    if request_method == 'POST':
+        print('--------------')
+        print(request.form)
+        print('--------------')
+        return redirect(url_for('name'))
+    return render_template('hello.html', request_method=request_method)
+
+
+@app.route('/name')
+def name():
+    return 'name page'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
